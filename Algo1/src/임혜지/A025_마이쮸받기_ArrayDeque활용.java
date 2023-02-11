7e2c67d41d2f2835af4b737641a1fc972c9a1476
hj <ihj028003@gmail.com>
2023-02-11 23:58:44 +0900

    25번 수정

--- Algo1/src/임혜지/A025_마이쮸받기_ArrayDeque활용.java
+++ Algo1/src/임혜지/A025_마이쮸받기_ArrayDeque활용.java
@@ -9,26 +9,24 @@ import java.util.Deque;
  * 0+0+1+...+n
  */
 public class A025_마이쮸받기_ArrayDeque활용 {
-	
+
 	public static void main(String[] args) {
-		Deque<Integer> p=new ArrayDeque<>();//사이즈 : 줄을 서는 사람들, 저장값 : 받아가는 마이쮸 개수
-		int n=20;//마이쮸개수 20개
-		int res=0;//마지막에 받은 사람
-		n-=1;//1번이 한개를 받고 시작
-		p.add(2);//1번이 2개를 받아야 함
-		while(n>0) {//n이 0이 될 때(마이쮸 다 나눠줄 때) 반복문 종료
-			for(int i=1;i<=p.size();i++) {
-			int temp=p.removeFirst();//그 사람이 받는 마이쮸 개수 꺼내서
-			n-=temp;//그만큼 나눠줌
-			if(n<=0) {//마이쮸 소진 시
-				res=i;//마지막 받은 사람 저장
-				break;//종료
-			}
-			temp++;//1개 증가
-			p.addLast(temp);//다음에는 1개 더 받도록 다시 줄을 섬
-			}
-			p.add(1);//새로운 사람 추가
+		Deque<Integer> p = new ArrayDeque<>();//줄을 서는 사람들
+		int n = 20;// 마이쮸개수 20개
+		int [] cnt=new int[100];//한명당 받는 개수
+		int idx=1;//새로 줄을 선 사람의 번호
+		int res = -1;// 현재 받은 사람
+		cnt[1]=1;//1번이 한개를 받아야 함 기록
+		p.add(1);// 1번이 줄을 섬
+		while (true) {// n이 0이 될 때(마이쮸 다 나눠줄 때) 반복문 종료
+			res=p.removeFirst();//앞에 줄은 선 사람이 나와서 마이쮸를 받을 차례
+			n-=cnt[res];//받아야하는 개수만큼 나눠줌
+			if(n<=0) break;//마이쮸 소진 시 반복문 탈출
+			cnt[res]++;//다음에 받아야할 마이쮸 1개 추가됨
+			p.addLast(res);//받아야하는 개수를 저장하며 다시 줄을 섬
+			p.addLast(++idx);//새로운 사람이 줄을 서는데, 
+			cnt[idx]=1;//1개부터 시작
 		}
-		System.out.println(res);//결과 출력
+		System.out.println(res);// 결과 출력
 	}
 }
