d09f1cc70353f122da74fcde439942c2b0929afc
hj <ihj028003@gmail.com>
2023-02-14 15:07:54 +0900

    33번, 35번

--- Algo1/src/임혜지/A033_SWEA1233_사칙연산유효성검사.java
+++ Algo1/src/임혜지/A033_SWEA1233_사칙연산유효성검사.java
@@ -1,5 +1,31 @@
 package 임혜지;
 
-public class A033_SWEA1233_사칙연산유효성검사 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class A033_SWEA1233_사칙연산유효성검사 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// 버퍼로 입력
+		StringBuilder str = new StringBuilder("");// 출력할 문자열
+		for (int test_case = 1; test_case <= 10; test_case++) {// 테스트 케이스 총 10개
+			int res = 1;// 유효성 변수(현재 유효한 상태이고, 유효하지 않은 상태를 만날 경우 0으로 바꾼 후 반복문 탈출)
+			int n = Integer.parseInt(in.readLine());// 노드 개수 입력
+			for (int i = 1; i <= n; i++) {// 총 n번의
+				String[] temp = in.readLine().split(" ");// 공백으로 구분해 입력
+				if(temp.length==4) {//자식 노드가 입력된 경우
+					if((int)(temp[1].charAt(0))>=48&&(int)(temp[1].charAt(0))<=57) {//숫자가 입력되었을 때
+						res=0;//유효하지 않음
+					}
+				}
+				else {//자식 노드가 입력되지 않은 경우
+					if((int)(temp[1].charAt(0))<48&&(int)(temp[1].charAt(0))>57) {//연산자가 입력되었을 때
+						res=0;//유효하지 않음
+					}
+				}
+			}
+			str.append("#").append(test_case).append(" ").append(res).append("\n");//출력문 덧붙임
+		}
+		System.out.println(str);//결과 출력
+	}
 }
--- Algo1/src/임혜지/A034_BJ16926_배열돌리기1.java
+++ Algo1/src/임혜지/A034_BJ16926_배열돌리기1.java
@@ -1,5 +1,34 @@
 package 임혜지;
 
-public class A034_BJ16926_배열돌리기1 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class A034_BJ16926_배열돌리기1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));//버퍼로 입력
+		StringTokenizer st=new StringTokenizer(in.readLine()," ");//첫째줄 입력(n,m,r)
+		int n=Integer.parseInt(st.nextToken());//배열의 크기(행 개수)
+		int m=Integer.parseInt(st.nextToken());//배열의 크기(열 개수)
+		int r=Integer.parseInt(st.nextToken());//회전해야 하는 횟수
+		int [][] arr=new int[n][m];//n by m 배열 생성
+		for(int i=0;i<n;i++) {//n줄에 걸쳐
+			st=new StringTokenizer(in.readLine()," ");//한 라인 입력 받고
+			for(int j=0;j<m;j++) {//열의 개수만큼
+				arr[i][j]=Integer.parseInt(st.nextToken());//배열값 저장
+			}
+		}
+		int [][] res=new int[n][m];//결과 저장할 배열
+		/*
+		 * 시작점은 [idx][idx] ... idx는 0부터 min(n,m)/2까지
+		 * 
+		 */
+		for(int i=0;i<r;i++) {//총 r번 회전
+				for(int j=0;j<Math.min(n,m)/2;j++) {//시작점은 [idx][idx] ... idx는 0부터 min(n,m)/2까지
+					
+				}
+			
+		}
+	}
 }
