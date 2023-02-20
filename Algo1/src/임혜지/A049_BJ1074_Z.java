53bbbf2459d8b8c7aeac236724f1b0117ce56799
hj <ihj028003@gmail.com>
2023-02-20 18:42:07 +0900

    Merge branch 'main' of https://lab.ssafy.com/s09/a19/03_algo/pair07_limhyeji_parkseungsoo.git into main

--- Algo1/src/임혜지/A049_BJ1074_Z.java
+++ Algo1/src/임혜지/A049_BJ1074_Z.java
@@ -7,8 +7,8 @@ import java.util.StringTokenizer;
 
 public class A049_BJ1074_Z {
 	static int n, r, c;// 입력받는 값(2의 n승이 이차원배열의 한 변, r행 c열의 순번 찾기)
-	static int size;
-	static int total = 0;
+	static int size;// 배열의 한 변
+	static int total = 0;// 결과로 출력할 방문 순번
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// 버퍼로 입력
@@ -19,22 +19,21 @@ public class A049_BJ1074_Z {
 
 		size = (int) Math.pow(2, n);// 배열의 크기
 
-		sol(0, size - 1, 0, size - 1, size);// 사각형 한칸이 (0,0)에서 (size-1,size-1)까지
+		sol(0, 0, size);// 사각형 한칸이 (0,0)에서 (size-1,size-1)까지
 	}
 
-	static void sol(int rStart, int rEnd, int cStart, int cEnd, int size) {
+	static void sol(int row, int col, int size) {
 		if (size == 1) {// 한칸일 경우
 			total++;// 순회 숫자 1 증가
-			if (rStart == r && cStart == c) {// 원하는 위치와 일치할 경우 출력
-				System.out.println(total);
+			if (row == r && col == c) {// 원하는 위치와 일치할 경우
+				System.out.println(total - 1);// 결과 출력
 			}
-			//System.out.println("total" + total + "번 : " + rStart + " " + rEnd + " " + cStart + " " + cEnd + " " + size);
 			return;
 		}
-
-		sol(rStart, rEnd / 2, cStart, cEnd / 2, size / 2);
-		sol(rStart, rEnd / 2, size / 2, cEnd, size / 2);
-		sol(size / 2, rEnd, cStart, cEnd / 2, size / 2);
-		sol(size / 2, rEnd, size / 2, cEnd, size / 2);
+		int newSize = size / 2;
+		sol(row, col, newSize);
+		sol(row, col + newSize, newSize);
+		sol(row + newSize, col, newSize);
+		sol(row + newSize, col + newSize, newSize);
 	}
 }
