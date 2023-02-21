5291f45fdabcfe17d565f684239cacd1e984ec4b
hj <ihj028003@gmail.com>
2023-02-21 16:44:42 +0900

    46번, 52번 주석 추가

--- Algo1/src/임혜지/A046_BJ16435_스네이크버드.java
+++ Algo1/src/임혜지/A046_BJ16435_스네이크버드.java
@@ -8,36 +8,36 @@ import java.util.StringTokenizer;
 
 public class A046_BJ16435_스네이크버드 {
 	static int n, l;// 과일 개수와 스네이크버드 초기 길이
-	static PriorityQueue<Integer> h = new PriorityQueue<Integer>();
+	static PriorityQueue<Integer> h = new PriorityQueue<Integer>();//높이를 오름차순 정리할 우선순위큐
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));//버퍼로 입력
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");//한 라인을 공백으로 입력받음
 
-		n = Integer.parseInt(st.nextToken());
-		l = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());//과일 개수
+		l = Integer.parseInt(st.nextToken());//스네이크버드 초기 길이
 
-		st = new StringTokenizer(in.readLine(), " ");
-		while (n-- > 0) {
-			h.add(Integer.parseInt(st.nextToken()));
+		st = new StringTokenizer(in.readLine(), " ");//두 번째 줄 입력(과일의 높이들)
+		while (n-- > 0) {//n개의
+			h.add(Integer.parseInt(st.nextToken()));//과일높이를 입력받아 우선순위 큐에 저장
 		}
 		
-		sol(0);
-		System.out.println(l);
+		sol(0);//큐의 0번부터 출발
+		System.out.println(l);//결과 도출된 스네이크버드 최종 길이 출력
 	}
 
 	static void sol(int idx) {
-		if(h.isEmpty()) {
-			return;
+		if(h.isEmpty()) {//만약 큐가 비었다면(더이상 과일이 없다면)
+			return;//재귀 종료
 		}
-		else if (h.peek() <= l) {
-			h.remove();
-			l += 1;
+		else if (h.peek() <= l) {//만약 먹을 수 있는 과일이라면(스네이크버드 길이보다 낮게 있다면)
+			h.remove();//과일 제거하면서
+			l += 1;//길이가 1 증가함
 		}
-		else {
-			return;
+		else {//더이상 먹을 수 없다면(모든 과일이 스네이크버드 길이보다 높다면)
+			return;//재귀 종료
 		}
 		
-		sol(idx + 1);
+		sol(idx + 1);//다음 과일 찾기
 	}
 }
--- Algo1/src/임혜지/A052_BJ1992_쿼드트리_과제9.java
+++ Algo1/src/임혜지/A052_BJ1992_쿼드트리_과제9.java
@@ -25,37 +25,37 @@ public class A052_BJ1992_쿼드트리_과제9 {
 
 		// 입력 끝
 		// -------------------------------------------------------
-		sol(0, 0, n, arr[0][0] == 0 ? true : false);// 시작하는 인덱스(i,j)와 사이즈, 시작점이 0인지 1인지 확인하는 플래그 전달
-		System.out.println(str.toString());
+		sol(0, 0, n, arr[0][0] == 0 ? true : false);// 시작하는 인덱스(row,col)와 사이즈, 시작점이 0인지 1인지 확인하는 플래그 전달
+		System.out.println(str.toString());//결과 출력
 
 	}
 
-	static boolean check(int row, int col, int size, boolean zeroFlag) {
-		for (int i = row; i < row + size; i++) {
+	static boolean check(int row, int col, int size, boolean zeroFlag) {//정사각형이 모두 같은 값인지 체크하는 메소드
+		for (int i = row; i < row + size; i++) {//(row,col)부터 확인하면서
 			for (int j = col; j < col + size; j++) {
-				if (zeroFlag == true && arr[i][j] == 1) {
-					return false;
+				if (zeroFlag == true && arr[i][j] == 1) {//만약 (row,col)의 값이 0이고 배열값이 1이라면
+					return false;//false 리턴
 				}
-				if (zeroFlag == false && arr[i][j] == 0) {
-					return false;
+				if (zeroFlag == false && arr[i][j] == 0) {//만약 (row,col)의 값이 1이고 배열값이 0이라면
+					return false;//false 리턴
 				}
 			}
 		}
-		return true;
+		return true;//모두 일치한다면 true 리턴
 	}
 	
-	static void sol(int row, int col, int size, boolean zeroFlag) {
-		if(check(row, col, size, zeroFlag)) {
-			str.append(zeroFlag==true?0:1);
-			return;
+	static void sol(int row, int col, int size, boolean zeroFlag) {//시작 인덱스(row,col)과 2분의 1씩할 사이즈, (row,col)이 0인지 1인지 확인할 플래그 전달
+		if(check(row, col, size, zeroFlag)) {//만약 그 정사각형의 값이 모두 일치한다면
+			str.append(zeroFlag==true?0:1);//그 값으로 압축하고
+			return;//재귀 종료
 		}
-		else{
-			str.append("(");
-			sol(row, col, size / 2, arr[row][col] == 0 ? true : false);
-			sol(row, col + size / 2, size / 2, arr[row][col + size / 2] == 0 ? true : false);
-			sol(row + size / 2, col, size / 2, arr[row + size / 2][col] == 0 ? true : false);	
-			sol(row + size / 2, col + size / 2, size / 2, arr[row + size / 2][col + size / 2] == 0 ? true : false);
-			str.append(")");
+		else{//정사각형의 값이 일치하지 않는다면 
+			str.append("(");//괄호를 열면서 4개의 칸으로 분리
+			sol(row, col, size / 2, arr[row][col] == 0 ? true : false);//왼쪽 위칸
+			sol(row, col + size / 2, size / 2, arr[row][col + size / 2] == 0 ? true : false);//오른쪽 위칸
+			sol(row + size / 2, col, size / 2, arr[row + size / 2][col] == 0 ? true : false);//왼쪽 아래칸
+			sol(row + size / 2, col + size / 2, size / 2, arr[row + size / 2][col + size / 2] == 0 ? true : false);//오른쪽 아래칸
+			str.append(")");//재귀 끝날 때 괄호 닫음
 		}
 	}
 }
