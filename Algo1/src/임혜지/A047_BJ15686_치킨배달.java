45c61df3d4c12b3224c562e08375dfae52f0edf5
hj <ihj028003@gmail.com>
2023-02-23 19:23:59 +0900

    47번,48번 진행 59번 완료

--- Algo1/src/임혜지/A047_BJ15686_치킨배달.java
+++ Algo1/src/임혜지/A047_BJ15686_치킨배달.java
@@ -8,57 +8,55 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
-	int i, j;
+class Node {//각 위치에 대한 클래스
+	int i, j;//행과 열 인덱스
 
-	Node(int i, int j) {
+	Node(int i, int j) {//행과 열 인덱스를 받는 클래스 생성자
 		this.i = i;
 		this.j = j;
 	}
 }
 
 public class A047_BJ15686_치킨배달 {
-	static int n, m;
+	static int n, m;//도시의 크기, 남는 치킨집 최대 개수
+	static Queue<Node> house = new LinkedList<>();//집 위치들을 저장할 큐
+	static Queue<Node> chicken = new LinkedList<>();//치킨집 위치들을 저장할 큐
+	static PriorityQueue<Integer> distance = new PriorityQueue<>();//거리를 오름차순으로 정리할 우선순위 큐
+
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));//버퍼로 입력
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");//한 라인 입력
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());//도시의 크기
+		m = Integer.parseInt(st.nextToken());//남는 치킨집 최대 개수 
 
-		Queue<Node> house = new LinkedList<>();
-		Queue<Node> chicken = new LinkedList<>();
-		PriorityQueue<Integer> distance = new PriorityQueue<>();
-
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(in.readLine(), " ");
-			for (int j = 1; j <= n; j++) {
-				int temp = Integer.parseInt(st.nextToken());
-				if (temp == 1) {
-					// 집 위치 저장
-					house.add(new Node(i, j));
+		
+		for (int i = 1; i <= n; i++) {//n by n 크기의 도시 입력을 받기 위해 n줄의
+			st = new StringTokenizer(in.readLine(), " ");//한 라인 입력받고
+			for (int j = 1; j <= n; j++) {//n열에 걸쳐
+				int temp = Integer.parseInt(st.nextToken());//한 칸의 기록 입력
+				if (temp == 1) {//집이라면
+					house.add(new Node(i, j));// 집 위치 저장
 				}
-				if (temp == 2) {
-					// 치킨집 위치 저장
-					chicken.add(new Node(i, j));
+				if (temp == 2) {//치킨집이라면
+					chicken.add(new Node(i, j));// 치킨집 위치 저장
 				}
 			}
 		}
-		int temp, min;
-		for (Node h : house) {
-			min = Integer.MAX_VALUE;
-			for (Node c : chicken) {
-				temp = Math.abs(h.i - c.i) + Math.abs(h.j - c.j);
-				min = Math.min(temp, min);
-			}
-			distance.add(min);
-		}
-		int res = 0;
-		System.out.println(distance);
+		int res = 0;//도시에 남은 m개의 치킨거리 저장할
+		//System.out.println(distance);
 		for (int i = 0; i < m; i++) {
 			res += distance.remove();
 		}
 		System.out.println(res);
+	}
+	
+	static int findMinD(Node h) {
+		int min=Integer.MAX_VALUE;
+		for(Node c:chicken) {
+			min=Math.min(min, Math.abs(h.i - c.i) + Math.abs(h.j - c.j));
+		}
+		return min;
 	}
 }
--- Algo1/src/임혜지/A059_BJ1987_알파벳_과제11.java
+++ Algo1/src/임혜지/A059_BJ1987_알파벳_과제11.java
@@ -3,90 +3,53 @@ package 임혜지;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class A059_BJ1987_알파벳_과제11 {
-	static int r, c;
-	static char[][] arr;
-	static int dirRow[] = { -1, 1, 0, 0 };
-	static int dirCol[] = { 0, 0, -1, 1 };
-	static int res = 0;
-	static List<Character> list;
+	static int r, c;// 보드의 세로, 가로
+	static char arr[][];//보드
+	static int check[] = new int[26];//알파벳 방문 여부
+	static int res = 0;//결과로 출력할 최대 이동횟수
+	static int dirR[] = { -1, 1, 0, 0 };//상하좌우
+	static int dirC[] = { 0, 0, -1, 1 };//상하좌우
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
 
-		list = new ArrayList<>();
+		String[] temp = in.readLine().split(" ");
+		r = Integer.parseInt(temp[0]);//보드의 세로 입력
+		c = Integer.parseInt(temp[1]);//보드의 가로 입력
 
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		arr = new char[r][c];
-		for (int i = 0; i < r; i++) {
-			String temp = in.readLine();
-			for (int j = 0; j < c; j++) {
-				arr[i][j] = temp.charAt(j);
+		arr = new char[r][c];//보드 생성
+
+		for (int i = 0; i < r; i++) {//r 줄에 걸쳐
+			String input = in.readLine();//문자열 입력받고
+			for (int j = 0; j < c; j++) {//c개의 문자를
+				arr[i][j] = input.charAt(j);//보드에 저장
 			}
 		}
-		list.add(arr[0][0]);
-		sol(0, 0, 1);
-		System.out.println(res);
+		// 입력끝
+		// ------------------------
+		check[arr[0][0] - 'A']++;//시작점의 알파벳 방문 횟수++
+		sol(0, 0, 1);//(0,0)에서 시작하고, 시작점을 지났으므로 1부터 카운트
+
+		System.out.println(res);//보드 이동 최대횟수 출력
 	}
 
-//	static void sol(int row,int col,int cnt) {
-//		
-//		for(int i=0;i<4;i++) {
-//			int nRow=row+dirRow[i];
-//			int nCol=col+dirCol[i];
-//			
-//			if(nRow>=0&&nRow<r&&nCol>=0&&nCol<c) {
-//				if(check(arr[nRow][nCol])) {
-//					list.add(arr[nRow][nCol]);
-//					int idx=list.indexOf(arr[nRow][nCol]);
-//					sol(nRow,nCol,cnt+1);
-//					list.remove(idx);
-//				}
-//			}
-//			res=Math.max(res, cnt);
-//		}
-//		return;
-//	}
+	static void sol(int row, int col, int cnt) {
 
-	static class Node {
-		int row, col;
+		for (int dir = 0; dir < 4; dir++) {//총 네 방향에 대해
+			int newR = row + dirR[dir];//상하좌우 인덱스 업데이트
+			int newC = col + dirC[dir];//상하좌우 인덱스 업데이트
 
-		public Node(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-	}
-
-	static void sol() {
-		Queue<Node> q = new ArrayDeque<>();
-		boolean visit[][] = new boolean[r][c];
-
-		q.offer(new Node(0, 0));
-		visit[0][0] = true;
-		Node cur = null;
-
-		while (!q.isEmpty()) {
-			cur = q.poll();
-
-			if (!visit[cur.row][cur.col]) {
-
+			if (newR >= 0 && newR < r && newC >= 0 && newC < c && check[arr[newR][newC] - 'A'] == 0) {
+				//보드 범위를 초과하지 않으면서 이미 방문하지 않은 알파벳에 대해
+				check[arr[newR][newC] - 'A']++;//방문횟수 1증가하고
+				sol(newR, newC, cnt + 1);//다음칸으로 이동
+				check[arr[newR][newC] - 'A']--;//또 다른 가짓수를 위해 방문횟수 1감소
 			}
 		}
-	}
+		res = Math.max(res, cnt);//네 방향에 대해 모두 방문 불가일 때, 그때까지의 이동횟수와 비교해 최대 이동횟수 업데이트
+		return;//재귀 종료
 
-	static boolean check(char c) {
-		for (char a : list) {
-			if (a == c)
-				return false;
-		}
-		return true;
 	}
 }
