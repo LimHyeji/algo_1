bdebbe11b54478bb1d02f2a13621dbc10ecffba7
hj <ihj028003@gmail.com>
2023-03-08 11:04:03 +0900

    58번 주석 완

--- Algo1/src/임혜지/A058_BJ17135_캐슬디펜스.java
+++ Algo1/src/임혜지/A058_BJ17135_캐슬디펜스.java
@@ -3,106 +3,143 @@ package 임혜지;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
- * 1. 궁수 세 명 "동시" 공격
- * 2. d 이하인 적 중에서 가장 가까운 적
- * 			-> 여럿일 경우 가장 왼쪽
- * 3. 공격 이후 적 한칸 아래 이동(1이 없을 경우 게임 끝)
- */
-
 public class A058_BJ17135_캐슬디펜스 {
-	static int n, m, d;// 맵의 행,열 크기, 거리
-	static char[][] map;// 맵
-	static Archer archer[] = new Archer[3];// 세 명의 궁수
-	static ArrayList<Enemy> enemy = new ArrayList<>();// 적의 리스트 생성
+	static class Node {// 행열 위치를 저장할 클래스
+		int row, col;
 
-	static int maxKill = 0;
+		Node(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
 
-	static class Node {// 위치 좌표 나타낼 클래스
-		int row, col;// 행과 열의 인덱스
-
-		public Node(int row, int col) {// Node의 생성자
+		void update(int row, int col) {// 객체의 위치 업데이트할 메소드
 			this.row = row;
 			this.col = col;
 		}
 	}
 
-	static class Archer extends Node {// 위치 좌표와 함께 궁수 정보 저장할 클래스
-
-		public Archer(int row, int col) {
-			super(row, col);
-		}
-	}
-
-	static class Enemy extends Node {// 위치 좌표와 함께 적 정보 저장할 클래스
-
-		public Enemy(int row, int col) {
-			super(row, col);
-		}
-	}
+	static int n, m, d;// 격자판 크기, 제한 거리
+	static int[][] arr;// 격자판
+	static int[] order;// 궁수 배치할 열 인덱스 순서
+	static int res;// 결과로 출력할 제거 가능 적의 최대수
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// 버퍼로 입력
-		StringTokenizer st = new StringTokenizer(in.readLine());// 한 줄 입력받고
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
 
-		n = Integer.parseInt(in.readLine());// 맵의 행 크기
-		m = Integer.parseInt(in.readLine());// 맵의 열 크기
-		d = Integer.parseInt(in.readLine());// 공격 가능 거리
-		// map = new char[n][m];//n by m 크기의 맵 생성
+		n = Integer.parseInt(st.nextToken());// 격자판 행 크기 입력
+		m = Integer.parseInt(st.nextToken());// 격자판 열 크기 입력
+		d = Integer.parseInt(st.nextToken());// 제한 거리 입력
 
-		for (int i = 0; i < n; i++) {// n개의 행 중에서
-			st = new StringTokenizer(in.readLine());// i번째 행 입력받고
-			for (int j = 0; j < m; j++) {// 총 m개의 열 중에서
-				map[i][j] = st.nextToken().charAt(0);// j번째 열 저장
-				if (map[i][j] == '1') {// 만약 적이라면
-					enemy.add(new Enemy(i, j));// 적의 리스트에 저장
-				}
+		arr = new int[n][m];// 격자판 생성
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());// 격자판 입력
 			}
+		}
+
+		// 입력 끝
+		// ----------------------------------
+		res = 0;// 제거 가능 적의 최대수를 구하기 위해 0으로 초기화
+		order = new int[3];// 궁수 배치 경우의 수 구할 배열 생성
+		order(0, 0);// 궁수 위치 구하기
+		System.out.println(res);// 결과 출력
+	}
+
+	static void order(int cnt, int start) {// 총 3개의 값을 구하기 위해 cnt로 체크, 조합 구하기 위한 start 변수
+		if (cnt == 3) {// 3번 재귀를 돌았을 경우
+			simul(order, copyArr());// 가지고 있는 궁수 배치 경우의 수로 시뮬레이션
+			return;// 재귀 종료
+		}
+
+		for (int i = start; i < m; i++) {
+			order[cnt] = i;
+			order(cnt + 1, i + 1);
 		}
 	}
 
-	static int cal(Node a, Node b) {// 두 위치 좌표의 거리를 계산할 메소드
+	static boolean check(int[][] arr) {// 현재 격자판에 적이 존재하는지 확인하는 메소드
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (arr[i][j] == 1)// 적이 존재한다면
+					return false;// false 리턴
+			}
+		}
+		return true;// 격자판에서 적이 모두 제외되었을 때 true 리턴
+	}
+
+	static int getDistance(Node a, Node b) {// 궁수와 적 사이의 거리를 구할 메소드
 		return Math.abs(a.row - b.row) + Math.abs(a.col - b.col);
 	}
 
-	static void com(int cnt, int start) {// 세 명의 궁수의 위치를 정할 조합 메소드
-		if (cnt == 3) {// 세 명의 위치가 완성되면
-			game();// 게임 시작
-			return;// 조합 종료
-		}
-		for (int i = start; i < m; i++) {
-			archer[cnt] = new Archer(n, i);// 세 명의 좌표를 정함(조합)
-			com(cnt + 1, i + 1);// 다음 궁수의 좌표 정하기 위해 재귀 호출
-		}
-	}
+	static Node getTarget(int order, int[][] arr) {// 제거 가능한 적 구할 메소드
+		Node archer = new Node(n, order);// 현재 궁수 위치
+		Node target = new Node(0, 0);// 적 위치 초기화
+		int distance = Integer.MAX_VALUE;// 거리의 최소값 구하기 위해 max 값 초기화
 
-	static void game() {
-		/*
-		 * 1. 거리가 d 이하인지 체크 2. 가장 가까운지 체크 3. 여러명이면 가장 왼쪽에 있는지 체크 4.
-		 * 
-		 * 우선순위 큐 : 정렬(거리-> 열인덱스) , 위치 저장해서 0 만들기 타입?
-		 * 
-		 * 
-		 * 1. 적은 한칸 아래 이동 2. 맵 벗어나면 (>=n이면) enemy-- 3.
-		 * 
-		 * -> 제거 가능 최대 수(전체 적 - 맵 벗어난 적) or (kill)
-		 */
-		while (enemy.size() > 0) {// 적이 있을 동안에만 턴
-
-			for (int i = n - 1; i >= 0; i--) {//행을 거꾸로 순회하면서
-				for (int j = 0; j < m; j++) {
-					if (i == n - 1 && map[i][j] == 1) {//만약 마지막 행에 적이 있을 경우에는
-						map[i][j] = 0;//적이 이동하므로, 0으로 바꾸고
-						//enemy.remove()--;
-					} else if (i != n - 1 && map[i][j] == 1) {
-						map[i + 1][j] = 1;
-						map[i][j] = 0;
-					}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				int curD = getDistance(new Node(i, j), archer);// 현재 궁수에서의 거리
+				if (arr[i][j] == 1 && curD < distance && curD <= d) {// 그 위치에 적이 존재하면서, 기존 최소 거리보다 가까우면서, 제한 거리 d이하이면
+					target.update(i, j);// 타겟 위치 업데이트
+					distance = curD;// 최소 거리 업데이트
+				} else if (arr[i][j] == 1 && curD == distance) {// 적이 존재하면서, 기존 최소 거리와 같은 거리에 있으면
+					if (j < target.col)// 현재 보고있는 적이 더 왼쪽에 있을 때
+						target.update(i, j);// 타겟 위치 업데이트
 				}
 			}
 		}
+
+		if (distance == Integer.MAX_VALUE) {// 타겟이 업데이트 되지 않았을 경우
+			target = null;// 타겟을 null로 초기화
+		}
+		return target;// 타겟 리턴
+	}
+
+	static void simul(int[] order, int[][] arr) {// 적이 모두 제외될 때까지 제거 가능 적의 최대수 업데이트할 메소드, 궁수 배치 가능한 경우의 수(조합)만큼 호출됨
+		int cnt = 0;// 현재 상황에서의 제거 가능 적의 수 카운트할 변수
+
+		while (!check(arr)) {// 적이 격자판에 존재할 때
+			Node[] targetList = new Node[3];// 세 명의 궁수에 대한 타겟리스트 생성하고
+			for (int i = 0; i < 3; i++) {
+				targetList[i] = getTarget(order[i], arr);// 타겟 구하기
+			}
+			for (int i = 0; i < 3; i++) {
+				if (targetList[i] != null) {// 제거 가능한 타겟이 있을 때
+					if (arr[targetList[i].row][targetList[i].col] != 0) {// 적이 이미 제거되지 않았다면
+						arr[targetList[i].row][targetList[i].col] = 0;// 제거 후
+						cnt++;// 제거 가능 적의 수 1 증가
+					}
+				}
+			}
+
+			move(arr);// 적이 한칸 아래씩 이동
+		}
+		res = Math.max(res, cnt);// 현재 상황에서의 제거 가능 적의 수가 더 작으면 업데이트
+	}
+
+	static void move(int[][] arr) {// 적이 한칸 아래로 이동할 메소드
+		for (int i = n - 1; i > 0; i--) {// 맨 아래줄부터 위에서 두번째 줄까지 총 n-1번 반복
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = arr[i - 1][j];// 자신의 윗칸에 있는 값으로 업데이트하고
+				arr[i - 1][j] = 0;// 자신의 윗칸은 0으로 초기화
+			}
+		}
+	}
+
+	static int[][] copyArr() {// 시뮬레이션을 여러번 돌리기 위해서 배열을 복사하는 메소드
+		int[][] arr2 = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				arr2[i][j] = arr[i][j];
+			}
+		}
+
+		return arr2;
 	}
 }
